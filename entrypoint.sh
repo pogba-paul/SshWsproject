7944e47636b5d53e15b3cd90acd163653a9ebd34
INKO_NNU <boucettay283@gmail.com>
2025-12-21 23:39:15 +0100

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# تعيين كلمة السر
+# 1. إعداد كلمة السر للسيرفر
 USER_PASS=${SSH_PASSWORD:-"root123"}
 echo "root:$USER_PASS" | chpasswd
 
-# تشغيل SSH
+# 2. تشغيل خدمة الـ SSH الأساسية
+mkdir -p /var/run/sshd
 /usr/sbin/sshd
 
-# تثبيت وتشغيل GOST (لتحويل المنافذ وقبول أي Payload)
+# 3. تحميل أداة Gost وتجهيزها
+# هذه الأداة ستعمل كمترجم لأي Payload ترسلها من التطبيق
 curl -L https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz | gunzip > /usr/local/bin/gost
 chmod +x /usr/local/bin/gost
 
-# تشغيل النفق على المنفذ 8080 ليدعم SSH و WS معاً
-/usr/local/bin/gost -L=ws://:8080?path=/
+# 4. تشغيل Gost لقبول Websocket و TCP و HTTP في نفس الوقت
+# هذا الأمر يجعل السيرفر يفهم الـ BMOVE والـ HEAD والروابط الخارجية
+/usr/local/bin/gost -L=ws://:8080?path=/ -L=tcp://:8080
