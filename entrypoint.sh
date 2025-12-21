89078098c84cb60bee80025b1434fbf0517b9090
INKO_NNU <boucettay283@gmail.com>
2025-12-21 21:49:46 +0100

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# تعيين كلمة سر للروت (يمكنك تغيير 'root123' لأي شيء تريده)
+# تعيين كلمة السر
 USER_PASS=${SSH_PASSWORD:-"root123"}
 echo "root:$USER_PASS" | chpasswd
 
-# تشغيل خدمة SSH
+# تشغيل SSH
 /usr/sbin/sshd
 
-# تشغيل Shellinabox لتحويل الـ SSH إلى Websocket على المنفذ 8080
-# هذا ما سيسمح لك بالاتصال عبر بروتوكول WS
-shellinaboxd -t -s /:SSH --port=8080 --disable-ssl
+# تثبيت وتشغيل GOST (لتحويل المنافذ وقبول أي Payload)
+curl -L https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz | gunzip > /usr/local/bin/gost
+chmod +x /usr/local/bin/gost
+
+# تشغيل النفق على المنفذ 8080 ليدعم SSH و WS معاً
+/usr/local/bin/gost -L=ws://:8080?path=/
